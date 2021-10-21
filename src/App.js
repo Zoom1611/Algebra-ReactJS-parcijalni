3060e591d2b28c8e88d80ef24233073bbec1d865
Ivan Razum <ivanrazum1611@gmail.com>
2021-10-21 21:11:56 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import InputForm from "./components/FormGitHubUser";
+
+/* function Clicker() {
+  const [counter, setCounter] = useState(0);
+
+  const updateCounter = () => {
+    setCounter((state) => state + 1);
+  }
+
+  return (
+    <button onClick={updateCounter}>Clicked {counter} time(s)</button>
+  )
+}
+
+ */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputForm />
+      {/* <Clicker /> */}
     </div>
   );
 }
